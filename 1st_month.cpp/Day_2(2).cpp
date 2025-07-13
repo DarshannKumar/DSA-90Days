2e2590fc8f6ac1ef949a186e03fa1963b746db29
DarshannKumar <darshandachu8385@gmail.com>
2025-07-13 15:38:29 +0530

    Day2 Qn:2

---
+++ 1st_month.cpp/Day_2(2).cpp
@@ -0,0 +1,32 @@
+// 561. Array Partition
+
+// Example 1:
+
+// Input: nums = [1,4,3,2]
+// Output: 4
+// Explanation: All possible pairings (ignoring the ordering of elements) are:
+// 1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+// 2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+// 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+// So the maximum possible sum is 4.
+// Example 2:
+
+// Input: nums = [6,2,6,5,1,2]
+// Output: 9
+// Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
+
+
+main key of this question is sorting we just pair every two elemt and add with first elelemt of pair 
+That is i=0+i=2+i=4+......
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i += 2) {//simple but efficient we use simple iteration of ith elemnt to next first elemnt that is i+2
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
